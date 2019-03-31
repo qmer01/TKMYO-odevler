977c810ab71d7556820b18616c1335af55410867
qmer01 <49024464+qmer01@users.noreply.github.com>
2019-03-31 21:48:25 +0300

    odevler
    odevler

---
+++ buton yarış oyunu/buton yarış oyunu/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace buton_yarış_oyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random a = new Random();
+            button1.Left += a.Next (0,10);
+            button2.Left += a.Next (0,10);
+            int x = this.ClientSize.Width;
+            int y = this.ClientSize.Height;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled==false)
+            {
+                timer1.Enabled = true;
+            }
+            else
+            {
+                timer1.Enabled = false;
+            }
+            //hocam bulamadım forma sınırlama olayını kusura bakmayın!
+        }
+    }
+}
---
+++ hesap makinesi/hesap makinesi/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace hesap_makinesi
+{
+    public partial class Form1 : Form
+    {
+        int sayı1;
+        int sayı2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sayı1 = Convert.ToInt32(textBox1.Text);
+            sayı2 = Convert.ToInt32(textBox2.Text);
+            label1.Text = Convert.ToString(sayı1 + sayı2);
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sayı1 = Convert.ToInt32(textBox1.Text);
+            sayı2 = Convert.ToInt32(textBox2.Text);
+            label1.Text = Convert.ToString(sayı1 - sayı2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            sayı1 = Convert.ToInt32(textBox1.Text);
+            sayı2 = Convert.ToInt32(textBox2.Text);
+            label1.Text = Convert.ToString(sayı1 / sayı2);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            sayı1 = Convert.ToInt32(textBox1.Text);
+            sayı2 = Convert.ToInt32(textBox2.Text);
+            label1.Text = Convert.ToString(sayı1 * sayı2);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("HOŞGELDİNİZ");
+
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            MessageBox.Show("YİNE BEKLERİZ");
+        }
+    }
+}
